8271e9b9fec7d289d04a826bbf33a0b436905b8f
anthonynassar <anthony21nassar@gmail.com>
2017-06-27 15:15:03 +0200

    Added abstraction layer for table cloud service

---
+++ PeopleApp/PeopleApp/Abstractions/ICloudService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleApp.Abstractions
+{
+    public interface ICloudService
+    {
+        ICloudTable<T> GetTable<T>() where T : TableData;
+    }
+}
