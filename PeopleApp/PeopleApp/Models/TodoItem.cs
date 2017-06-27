9de0b7cb07ee713a66bd9ae66c1500523811c03f
anthonynassar <anthony21nassar@gmail.com>
2017-06-27 15:24:44 +0200

    Added the todoitem model

---
+++ PeopleApp/PeopleApp/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using PeopleApp.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleApp.Models
+{
+    public class TodoItem : TableData
+    {
+        public string Text { get; set; }
+        public bool Complete { get; set; }
+    }
+}
