b8071aa8655ad7f2444f2d48654b3e910d1bc578
anthonynassar <anthony21nassar@gmail.com>
2017-06-27 15:22:45 +0200

    Implement the ICloudService interface

---
+++ PeopleApp/PeopleApp/Services/AzureCloudService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.WindowsAzure.MobileServices;
+using PeopleApp.Abstractions;
+
+namespace PeopleApp.Services
+{
+    public class AzureCloudService : ICloudService
+    {
+        MobileServiceClient client;
+
+        public AzureCloudService()
+        {
+            client = new MobileServiceClient("https://peopleapp3.azurewebsites.net/");
+        }
+
+        public ICloudTable<T> GetTable<T>() where T : TableData
+        {
+            return new AzureCloudTable<T>(client);
+        }
+    }
+}
