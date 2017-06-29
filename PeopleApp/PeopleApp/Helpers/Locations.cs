627ca7a91a24b27a711c4768be73b34aa02ab4be
anthonynassar <anthony21nassar@gmail.com>
2017-06-29 12:04:06 +0200

    Configured local debugging

---
+++ PeopleApp/PeopleApp/Helpers/Locations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeopleApp.Helpers
+{
+    public static class Locations
+    {
+#if DEBUG
+        public static readonly string AppServiceUrl = "http://localhost:52192/";
+        public static readonly string AlternateLoginHost = "https://peopleapp3.azurewebsites.net";
+#else
+        public static readonly string AppServiceUrl = "https://peopleapp3.azurewebsites.net";
+        public static readonly string AlternateLoginHost = null;
+#endif
+    }
+}
--- PeopleApp/PeopleApp/Services/AzureCloudService.cs
+++ PeopleApp/PeopleApp/Services/AzureCloudService.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.MobileServices;
 using PeopleApp.Abstractions;
 using Xamarin.Forms;
+using PeopleApp.Helpers;
 
 namespace PeopleApp.Services
 {
@@ -12,7 +13,9 @@ namespace PeopleApp.Services
 
         public AzureCloudService()
         {
-            client = new MobileServiceClient("https://peopleapp3.azurewebsites.net/");
+            client = new MobileServiceClient(Locations.AppServiceUrl);
+            if (Locations.AlternateLoginHost != null)
+                client.AlternateLoginHost = new Uri(Locations.AlternateLoginHost);
         }
 
         public ICloudTable<T> GetTable<T>() where T : TableData => new AzureCloudTable<T>(client);
