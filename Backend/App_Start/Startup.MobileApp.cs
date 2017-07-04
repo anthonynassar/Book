2a5e128ecc135f172b3b89a15ab5f4ceec9d2a98
anthonynassar <anthony21nassar@gmail.com>
2017-07-04 12:19:41 +0200

    Configured code first migration

--- Backend/App_Start/Startup.MobileApp.cs
+++ Backend/App_Start/Startup.MobileApp.cs
@@ -9,6 +9,7 @@ using Microsoft.Azure.Mobile.Server.Config;
 using Backend.DataObjects;
 using Backend.Models;
 using Owin;
+using System.Data.Entity.Migrations;
 
 namespace Backend
 {
@@ -25,8 +26,12 @@ namespace Backend
                 //.UseDefaultConfiguration()
                 .ApplyTo(config);
 
-            // Use Entity Framework Code First to create database tables based on your DbContext
-            Database.SetInitializer(new MobileServiceInitializer());
+            // Automatic Code First Migrations
+            var migrator = new DbMigrator(new Migrations.Configuration());
+            migrator.Update();
+
+            //// Use Entity Framework Code First to create database tables based on your DbContext
+            //Database.SetInitializer(new MobileServiceInitializer());
 
             //MobileAppSettingsDictionary settings = config.GetMobileAppSettingsProvider().GetMobileAppSettings();
 
@@ -46,24 +51,6 @@ namespace Backend
             app.UseWebApi(config);
         }
     }
-
-    public class MobileServiceInitializer : CreateDatabaseIfNotExists<MobileServiceContext>
-    {
-        protected override void Seed(MobileServiceContext context)
-        {
-            List<TodoItem> todoItems = new List<TodoItem>
-            {
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
-                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false }
-            };
-
-            foreach (TodoItem todoItem in todoItems)
-            {
-                context.Set<TodoItem>().Add(todoItem);
-            }
-
-            base.Seed(context);
-        }
-    }
+    
 }
 
---
+++ Backend/Migrations/Configuration.cs
@@ -0,0 +1,53 @@
+namespace Backend.Migrations
+{
+    using Backend.DataObjects;
+    using Backend.Models;
+    using Microsoft.Azure.Mobile.Server.Tables;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Backend.Models.MobileServiceContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            SetSqlGenerator("System.Data.SqlClient", new EntityTableSqlGenerator());
+            ContextKey = "Backend.Models.MobileServiceContext";
+        }
+
+        protected override void Seed(MobileServiceContext context)
+        {
+            List<TodoItem> todoItems = new List<TodoItem>
+            {
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "First item", Complete = false },
+                new TodoItem { Id = Guid.NewGuid().ToString(), Text = "Second item", Complete = false }
+            };
+
+            foreach (TodoItem todoItem in todoItems)
+            {
+                context.Set<TodoItem>().Add(todoItem);
+            }
+
+            base.Seed(context);
+        }
+
+        //protected override void Seed(Backend.Models.MobileServiceContext context)
+        //{
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        //}
+    }
+}
